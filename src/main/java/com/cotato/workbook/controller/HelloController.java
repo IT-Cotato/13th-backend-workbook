ce154ce20ea1e11336dbcb90ccf5f8e6a034abb4
Junseung-Ock <junseung0305@kyonggi.ac.kr>
2026-03-09 17:00:37 +0900

    feat: HelloController 기본 응답 추가

---
+++ src/main/java/com/cotato/workbook/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.cotato.workbook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "안녕하세요, 옥준승 입니다!";
+    }
+}
