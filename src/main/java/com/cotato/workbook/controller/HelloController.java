508acfe8c27694deb47c25e62982f3fed1482bf3
syeon111 <psooy19@gmail.com>
2026-03-14 14:02:13 +0900

    feat: HelloController 기본 응답 추가

---
+++ src/main/java/com/cotato/workbook/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cotato.workbook.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "안녕하세요, 박수연입니다!";
+    }
+}
