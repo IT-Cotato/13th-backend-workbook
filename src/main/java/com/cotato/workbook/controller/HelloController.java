fe8af26f60f4050bbd020d7094622c6502db157f
Haewon Lee <ttoodong@naver.com>
2026-03-15 21:33:42 +0900

    feat: Swagger 설정 및 UserController 문서화

--- src/main/java/com/cotato/workbook/controller/HelloController.java
+++ src/main/java/com/cotato/workbook/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "안녕하세요, 임준서 입니다!";
+        return "안녕하세요, 이해원입니다!";
     }
 }
---
+++ src/main/java/com/cotato/workbook/domain/user/dto/UserResponse.java
@@ -0,0 +1,12 @@
+package com.cotato.workbook.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor // 모든 필드를 받는 생성자를 자동으로 만들어줘요
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+}
