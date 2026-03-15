63b47adca1b2dbc189b1b7f8833d5e837c520387
hyeonszz <shj270515@gmail.com>
2026-03-15 14:30:11 +0900

    feat: Swagger 설정 및 UserController 문서화

---
+++ src/main/java/com/cotato/workbook/domain/user/dto/UserRequest.java
@@ -0,0 +1,10 @@
+// UserRequest.java — 유저 생성/수정 시 클라이언트에서 받아오는 데이터
+package com.cotato.workbook.domain.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRequest {
+    private String name;
+    private String email;
+}
