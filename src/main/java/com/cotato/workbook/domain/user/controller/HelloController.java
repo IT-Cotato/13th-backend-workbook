d14cfe79040e8596add9ae5e9f8acc8e8a2deeab
Cuyqo <saon0410@gmail.com>
2026-03-18 22:29:40 +0900

    feat: Swagger 설정 및 UserController 문서화

--- src/main/java/com/cotato/workbook/domain/user/controller/HelloController.java
+++ src/main/java/com/cotato/workbook/domain/user/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.cotato.workbook.controller;
+package com.cotato.workbook.domain.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
