b43c861fb5229e6c66557bd91d8ba43fb7a4f290
Baeseona <baeseona2930@gmail.com>
2026-03-31 23:41:06 +0900

    feat: EC2 배포 및 환경 변수 관리 구현

--- src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
+++ src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
@@ -2,18 +2,25 @@ package com.cotato.workbook.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.title}")
+    private String swaggerTitle;
+
+    @Value("{swagger.description}")
+    private String swaggerDescription;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("My API")                    // 문서 제목
-                        .description("스터디 실습용 API 문서")  // 설명
-                        .version("v1.0.0"));                  // 버전
+                        .title(swaggerTitle)                    // 문서 제목
+                        .description(swaggerDescription));
+
     }
 }
