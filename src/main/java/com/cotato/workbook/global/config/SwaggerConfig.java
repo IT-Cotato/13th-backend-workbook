1f1d38e8f0cc8beb890ab413f825a451a54857c3
chaeyylee <ivystar1129@naver.com>
2026-04-01 16:39:15 +0900

    feat: EC2 배포 및 환경 변수 관리 구현

--- src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
+++ src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
@@ -4,16 +4,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.title}")
+    private String swaggerTitle;
+
+    @Value("${swagger.description}")
+    private String swaggerDescription;
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("My API")                    // 문서 제목
-                        .description("스터디 실습용 API 문서")  // 설명
-                        .version("v1.0.0"));                  // 버전
+                        .title(swaggerTitle)
+                        .description(swaggerDescription));
     }
 }
