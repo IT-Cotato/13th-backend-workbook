162791b47c218cab5c3102c21e65fd8f99be2850
김서윤 <sy0518@gimseoyun-ui-MacBookAir.local>
2026-04-02 13:52:50 +0900

    feat: EC2 배포 및 환경 변수 관리 구현

--- src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
+++ src/main/java/com/cotato/workbook/global/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.cotato.workbook.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.title}")
+    private String swaggerTitle;
+
+    @Value("${swagger.description}")
+    private String swaggerDescription;
 
     @Bean
     public OpenAPI openAPI() {
